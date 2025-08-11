53e3df1ba70b53bb5254b496b099a2a8cef720f3
Ghostar <gillomdev@gmail.com>
2025-08-10 22:09:38 -0700

    2nd Level FinalVersion

--- LevelScenes/Level1/Level1Scenes/Level1Scripts/pause.gd
+++ LevelScenes/Level1/Level1Scenes/Level1Scripts/pause.gd
@@ -1,22 +1,30 @@
 extends Control
 
+@onready var player = get_tree().current_scene.get_node("Player")
+
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("stop"):
+	if Input.is_action_just_pressed("stop"):  # Ejemplo: tecla ESC para pausar/resumir
 		pause_or_unpause()
-		
-		
+	if Input.is_action_just_pressed("save_game"):  # Ejemplo: tecla Q para guardar
+		save_game()
+
 func pause_or_unpause():
-	if get_tree().paused == true:
-		$".".hide()
+	if get_tree().paused:
+		$"." .hide()
 		get_tree().paused = false
-	elif get_tree().paused == false:
-		$".".show()
+	else:
+		$"." .show()
 		get_tree().paused = true
 
 func _on_pause_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_esc_pressed() -> void:
 	pause_or_unpause()
-	#function body.
+
+func _on_Save_pressed() -> void:
+	save_game()
+
+func save_game():
+	if player:
+		SaveManager.save_game(player.global_position)
+		print("Partida guardada!")
+	else:
+		print("Jugador no encontrado")
