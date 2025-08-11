65814a73c94bbf96b9f64e55a724c2373bf8bbf3
Ghostar <gillomdev@gmail.com>
2025-08-10 18:14:51 -0700

    2nd Level Clear

--- LevelScenes/Level2/Level2Scripts/finish.gd
+++ LevelScenes/Level2/Level2Scripts/finish.gd
@@ -1,17 +1,20 @@
 extends Area2D
 
 @export var next_level_scene := "res://LevelScenes/Level1/Level1Scenes/level_1.tscn"
-@export var required_cherries := 5
+@export var required_cherries := 7
+
+@onready var message_label := $Advice
 
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
+	message_label.text = ""
 
 func _on_body_entered(body):
 	if body.name == "Player":
 		if Global.cherries >= required_cherries:
+			message_label.text = ""
 			print("Ganaste")
-			Global.cherries = 0  # Resetea cherries al ganar
-			Global.collected_cherries.clear()  # Limpia las monedas recogidas para que vuelvan a aparecer
+			Global.reset()  # Resetea cherries y collected_cherries
 			get_tree().change_scene_to_file(next_level_scene)
 		else:
-			print("Te faltan cherries para ganar. Tienes:", Global.cherries)
+			message_label.text = "Te faltan Cherries para ganar. Tienes: %d" % Global.cherries
